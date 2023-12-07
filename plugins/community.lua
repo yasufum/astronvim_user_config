a6301a308fe5b2b05816cf710fa6891b80dda4e3
Yasufumi Ogawa <yasufum.o@gmail.com>
2023-12-07 11:29:32 +0900

    Add catppuccin colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,24 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.colorscheme.rose-pine" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { -- further customize the options set by the community
+    "catppuccin",
+    opts = {
+      integrations = {
+        sandwich = false,
+        noice = true,
+        mini = true,
+        leap = true,
+        markdown = true,
+        neotest = true,
+        cmp = true,
+        overseer = true,
+        lsp_trouble = true,
+        rainbow_delimiters = true,
+      },
+    },
+  },
 }
